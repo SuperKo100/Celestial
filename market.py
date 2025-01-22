b8436eb7d664e0ca06551573a4352563a2c70b26
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-01-22 17:50:08 +0800

    all

--- market.py
+++ market.py
@@ -2,6 +2,6 @@ import streamlit as st
 import yfinance as yf
 
 tickers = ["TSM", "NVDA", "IWY", "GLD"]
-history = yf.download(tickers)
+history = yf.download(tickers, period="10y")
 
 st.dataframe(history)
