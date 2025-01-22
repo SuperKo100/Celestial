0433d98378a49ec02e1ad2fc2c4bbf52916278e9
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-01-22 17:56:51 +0800

    all

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
-pg = st.navigation([
-    st.Page("market.py", title="Market", icon=":material/add_circle:"),
-    # st.Page("delete.py", title="Delete entry", icon=":material/delete:")
-])
+pg = st.navigation(
+    {
+        "Market": [
+            st.Page("market.py", title="Market", icon=":material/add_circle:"),
+            # st.Page("delete.py", title="Delete entry", icon=":material/delete:")
+        ],
+    }
+)
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
 
