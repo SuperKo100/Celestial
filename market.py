877c0d84cef7789b8d756f56f25ecc341833c963
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-01-22 17:42:59 +0800

    all

--- market.py
+++ market.py
@@ -1,31 +1,7 @@
 import streamlit as st
 import yfinance as yf
 
-ticker = st.selectbox(
-    "Select a ticker",
-    ["TSM", "NVDA", "IWY", "GLD"]
-)
+tickers = ["TSM", "NVDA", "IWY", "GLD"]
+history = yf.download(tickers)
 
-tk = yf.Ticker(ticker)
-
-# CALL THE MULTIPLE FUNCTIONS AVAILABLE AND STORE THEM IN VARIABLES.
-actions = tk.get_actions()
-analysis = tk.get_analysis()
-balance = tk.get_balance_sheet()
-calendar = tk.get_calendar()
-cashflow = tk.get_cashflow()
-info = tk.get_info()
-inst_holders = tk.get_institutional_holders()
-news = tk.get_news()
-recommendations = tk.get_recommendations()
-sustainability = tk.get_sustainability()
-
-st.dataframe(actions)
-st.dataframe(analysis)
-st.dataframe(balance)
-st.dataframe(calendar)
-st.dataframe(cashflow)
-st.dataframe(info)
-st.dataframe(news)
-st.dataframe(recommendations)
-st.dataframe(sustainability)
+st.dataframe(history)
