e4c256060afa6b659e57add4cb4ba990aefdffe1
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-02-07 11:11:21 +0800

    all

--- market.py
+++ market.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import streamlit.components.v1 as components
+# import streamlit.components.v1 as components
 import yfinance as yf
 
-# Include Google Analytics tracking code
-with open("google_analytics.html", "r") as f:
-    html_code = f.read()
-    components.html(html_code, height=0)
+# # Include Google Analytics tracking code
+# with open("google_analytics.html", "r") as f:
+#     html_code = f.read()
+#     components.html(html_code, height=0)
 
 tickers = ["TSM", "NVDA", "IWY", "GLD"]
 history = yf.download(tickers, period="10y")
