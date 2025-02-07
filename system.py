fd7233a20c5a9e29136ae71d94aef6990b1f80f5
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-02-07 11:20:54 +0800

    all

--- system.py
+++ system.py
@@ -1,7 +1,1 @@
 import streamlit as st
-# import streamlit.components.v1 as components
-
-# # Include Google Analytics tracking code
-# with open("google_analytics.html", "r") as f:
-#     html_code = f.read()
-#     components.html(html_code, height=0)
