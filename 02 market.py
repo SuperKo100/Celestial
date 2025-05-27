f9de67d74d9048fc7309a5551b7d4d90bc472ec6
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-05-27 17:29:30 +0800

    all

--- 02 market.py
+++ 02 market.py
@@ -5,3 +5,21 @@ tickers = ["TSM", "NVDA", "IWY", "GLD"]
 history = yf.download(tickers, period="10y")
 
 st.dataframe(history[["Close", "Volume"]])
+
+# Create a graphlib graph object
+graph = graphviz.Digraph()
+graph.edge("run", "intr")
+graph.edge("intr", "runbl")
+graph.edge("runbl", "run")
+graph.edge("run", "kernel")
+graph.edge("kernel", "zombie")
+graph.edge("kernel", "sleep")
+graph.edge("kernel", "runmem")
+graph.edge("sleep", "swap")
+graph.edge("swap", "runswap")
+graph.edge("runswap", "new")
+graph.edge("runswap", "runmem")
+graph.edge("new", "runmem")
+graph.edge("sleep", "runmem")
+
+st.graphviz_chart(graph)
