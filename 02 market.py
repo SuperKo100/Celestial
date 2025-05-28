3abd27dc2ab639ef573b89c13cf37c56fffb3213
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-05-28 18:16:31 +0800

    requirements

--- 02 market.py
+++ 02 market.py
@@ -1,13 +1,6 @@
 import streamlit as st
-import pandas as pd
-import pandas_datareader as pdr
 import graphviz
 
-@st.cache_data
-def getdata(s: str) -> pd.DataFrame:
-    return pdr.get_data_fred(s)
-
-st.DataFrame(getdata("GS10"))
 
 # Create a graphlib graph object
 graph = graphviz.Digraph()
