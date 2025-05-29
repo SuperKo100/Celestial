5e8927234b50bb866531fdc4946149ad8993de52
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-05-29 16:37:20 +0800

    all

--- 02 market.py
+++ 02 market.py
@@ -15,9 +15,27 @@ def getdata(s: str) -> pd.DataFrame:
     y.set_index("date", inplace=True)
     return y
 
-data = getdata("TRESEGCNM052N")
+indices = {
+    "US Treasury 30Y Constant Murity": "DGS30",
+    "US Treasury 20Y Constant Murity": "DGS20",
+    "US Treasury 10Y Constant Murity": "DGS10",
+    "US Treasury 7Y Constant Murity": "DGS7",
+    "US Treasury 5Y Constant Murity": "DGS5",
+    "US Treasury 3Y Constant Murity": "DGS3",
+    "US Treasury 2Y Constant Murity": "DGS2",
+    "US Treasury 1Y Constant Murity": "DGS1",
+    "US Treasury 6M Constant Murity": "DGS6MO",
+    "US Treasury 3M Constant Murity": "DGS3MO",
+    "US Treasury 1M Constant Murity": "DGS1MO",
+}
+
+index = st.selectbox("Select an index",
+                     indices.keys())
+data = getdata("indices[index]")
+st.write(f"You select {index} and its symbol is {indices[index]}")
 st.write(data)
 st.line_chart(data)
+
 
 # Create a graphlib graph object
 graph = graphviz.Digraph()
