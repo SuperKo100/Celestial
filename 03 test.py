ee8cb7241a4444172147b835144cea9a81ea0465
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-06-04 14:53:15 +0800

    all

--- 03 test.py
+++ 03 test.py
@@ -11,6 +11,8 @@ def getdata(s: str) -> dict:
     """
     data = yf.download([s, "^SPX"], period="ytd", interval="1wk", multi_level_index=False)
     data.columns = data.columns.to_flat_index().str.join('_')
+    data = data[[f"Open_{s}", f"High_{s}", f"Low_{s}", f"Close_{s}", f"Volume_{s}",
+                "Open_^SPX", "High_^SPX", "Low_^SPX", "Close_^SPX", "Volume_^SPX"]]
     data = data.fillna(method="ffill").replace(0, method="ffill")
     return data
 
