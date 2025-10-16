f59baf2c1c153d01cece3a949e34b841d1dea2af
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-10-16 16:35:32 +0800

    all

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import streamlit.components.v1 as components
-
-# Include Google Analytics tracking code
-with open("google_analytics.html", "r") as f:
-    html_code = f.read()
-    components.html(html_code, height=0)
 
 pg = st.navigation(
     {
