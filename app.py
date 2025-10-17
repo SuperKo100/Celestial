2dcef2bcbced07999fb16f508a88c6db894e3bdf
Ko Tseng <49175944+SuperKo100@users.noreply.github.com>
2025-10-17 09:13:38 +0800

    all

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import streamlit as st
 pg = st.navigation(
     {
         "Apps": [
-            st.Page("01 analyzer.py", title="Analyzer", icon=":material/add_circle:"),
-            st.Page("02 market.py", title="Market", icon=":material/add_circle:"),
-            st.Page("03 test.py", title="Test", icon=":material/add_circle:"),
-            st.Page("04 topics.py", title="Topics", icon=":material/add_circle:"),
+            st.Page("01 screener.py", title="Analyzer", icon=":material/add_circle:"),
+            st.Page("02 analyzer.py", title="Market", icon=":material/add_circle:"),
+            st.Page("03 market.py", title="Test", icon=":material/add_circle:"),
         ]
     }
 )
